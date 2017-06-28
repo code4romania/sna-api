b112e89bfa958266bc2ebe52526883c8e268af04
Costea Alexandru <costeaalex@yahoo.com>
2017-06-28 19:21:21 +0300

    Population for counties in answers.

--- app/Api/V1/DataBuilders/Answers/CountyInstitutionBuilder.php
+++ app/Api/V1/DataBuilders/Answers/CountyInstitutionBuilder.php
@@ -3,8 +3,21 @@
 namespace App\Api\V1\DataBuilders\Answers;
 
 use App\AnswersCounty;
+use App\County;
+use App\Api\V1\Institutions\Institution;
 
 class CountyInstitutionBuilder extends Builder {
+    
+    public function getAnswersFor(Institution $institution) {
+        $output = parent::getAnswersFor($institution);
+        $output['employees'] = $this->getEmployeesFor($institution);
+        return $output;
+    }
+    
+    protected function getEmployeesFor($institution) {
+        $county = County::where('id', $institution->getId())->first();
+        return $county->population;
+    }
     
     protected function getAnswerRowsFor($institutionId, $questionId) {
         return AnswersCounty::where('question_id', $questionId)->where('institution_id', $institutionId)

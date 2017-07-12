f55b2917666555f6189560b135e2d9e2a868a68e
Catalin Tudorache <tudorache.catalin47@gmail.com>
2017-07-12 19:53:59 +0300

    Added population by years in /county

--- app/Api/V1/Transformers/CountyTransformer.php
+++ app/Api/V1/Transformers/CountyTransformer.php
@@ -3,16 +3,25 @@
 namespace App\Api\V1\Transformers;
 
 use App\County;
+use App\CountyPopulation;
 
 class CountyTransformer extends Transformer
 {
     public function transform($county)
     {
+        $countyPopulationRows = CountyPopulation::where('county_id', $county->id)->get();
+        $countyPopulation = array();
+
+        foreach($countyPopulationRows as $row) {
+            $countyPopulation[] = array('population' => $row->population,
+                                        'year' => $row->year);
+        }
+
         return [
             'id' => $county->id,
             'name' => $county->name,
             'code' => $county->code,
-            'population' => $county->population
+            'population' => $countyPopulation
         ];
     }
 }

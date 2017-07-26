6bfcd0fd316868c45caf58903b56c98240bac866
alex <alex@localhost.localdomain>
2017-07-26 17:32:58 +0300

    New import files for code4 import from sna.

--- app/Api/V1/Institutions/Institution.php
+++ app/Api/V1/Institutions/Institution.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Api\V1\Institutions;
 
-interface Institution {
-    
+interface Institution
+{
+
     public function getId();
-    
+
     public function getName();
-    
 }

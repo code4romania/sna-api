5c64a4c8556ddfe9d9df0f6c8642c93e1752a2ad
Costea Alexandru <costeaalex@yahoo.com>
2017-09-06 19:52:31 +0300

    Excepted Ministry and AntiCorruption from /ansers/counties response.

--- app/Api/V1/Controllers/AnswersController.php
+++ app/Api/V1/Controllers/AnswersController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Api\V1\Controllers;
 
 use App\Http\Controllers\Controller;
@@ -11,17 +10,26 @@ use App\Api\V1\Institutions\InstitutionsProvider;
 
 class AnswersController extends Controller
 {
-    use Headers;
+
+    private $bogusCountyCodesToExcludeFromCountyAnswer = array(
+        'AI',
+        'MI'
+    );
     
+    use Headers;
+
     public function listByInstitutionType($institutionType)
     {
         $answersBuilder = AnswersBuilderSelector::getByInstitution($institutionType);
         $institutions = InstitutionsProvider::getInstitutionsFor($institutionType);
         $output = array();
         foreach ($institutions as $institution) {
+            if ($institutionType == AnswersBuilderSelector::COUNTY_TYPE && in_array($institution->code, $this->bogusCountyCodesToExcludeFromCountyAnswer)) {
+                continue;
+            }
             $output[] = $answersBuilder->getAnswersFor($institution);
         }
-
+        
         return response()->json($output, 200, $this->getHeaders());
     }
 }
